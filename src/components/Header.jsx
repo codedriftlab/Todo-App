31c7beea21848de22fc4a3d79c1f614d5658136b
codedriftlab <tempmail3470@gmail.com>
2026-03-30 16:46:13 +0530

    add style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-function  Header() {
-    return (
-        <div className="header">
-            <h1>Todo App</h1>
-        </div>
-    );
+import { useState } from "react";
+
+function Header({ addTodo }) {
+  const [input, setInput] = useState("");
+
+  const handleAdd = () => {
+    if (!input) return;
+    addTodo(input);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <h1>To-Do App</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter task..."
+      />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
 }
 
-export default Header;  
+export default Header;
